541dd95ec883b0535f8faf5f31d4e0fec8de7109
edouardyonga <ededwardyonga@gmail.com>
2023-03-28 11:42:16 -0400

    feat(delete): softDelete on users and jobs with migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['company', 'user_id','title', 'email', 'website', 'tags',  'location', 'description', 'logo'];
 
